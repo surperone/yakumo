f6a4735470d254ed1edf790456973a49978f6c23
八云酱 <ryanwschina@users.noreply.github.com>
2017-03-13 10:01:27 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 	<footer class="site-footer clearfix ">
 	    <section class="copyright "><b><a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a> &copy; <?php echo date("Y"); ?></b></section>
-	    <section><b>本主题由<a href="https://www.bayun.org" target="_blank"> 八云酱 </a>编写</b></section>  
-	    <section><b>主题样式<a href="https://luolei.org" target="_blank"> 罗磊的独立博客 </a></b></section>
+	    <section><b>主题作者<a href="https://www.bayun.org" target="_blank"> 八云酱 </a></b></section>  
+	    <section><b>样式作者<a href="https://luolei.org" target="_blank"> 罗磊 </a></b></section>
 	    <section><b>京ICP备16042939号</b></section>    
 	</footer>  
 	<?php $this->footer(); ?>
